486895ce1e85609580f4c488b295505a057e7605
Sophie Moerman <sophie.moerman1@student.ehb.be>
2021-12-24 03:39:26 +0100

    delete start.js

--- api/src/start.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const app = require("./server.js");
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`example app listening at http://localhost:${port}`);
-});
